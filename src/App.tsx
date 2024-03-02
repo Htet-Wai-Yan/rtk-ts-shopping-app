f87c8de1fdc96e41d9ff7d2603113cfdec02e011
htet-wai-yan <h.waiyan101@gmail.com>
2024-03-02 16:33:26 +0630

    setup project and theme

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-3xl text-blue-300 font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        goldenRose: {
+          primary: "#eab308",
+          secondary: "#fda4af",
+          accent: "#eab308",
+          neutral: "#f5f5f4",
+          "base-100": "#e5e7eb",
+          info: "#0066ff",
+          success: "#50aa00",
+          warning: "#f97316",
+          error: "#e2314b",
+        },
+      },
+      // "light",
+      // "dark",
+    ],
+  },
+  plugins: [require("daisyui")],
+};
